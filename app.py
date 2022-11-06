4088e3090419baf40652afb793be200fec3caa24
PranathiJyothi <62240579+PranathiJyothi@users.noreply.github.com>
2022-11-06 18:53:37 +0530

    Update app.py

--- app.py
+++ app.py
@@ -2,6 +2,6 @@ from flask import Flask
 app = Flask(__name__)
 @app.route('/')
 def home():
-    return 'Home Page!!'
+    return 'Home Page!'
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=8070, debug=False)
